dfdf19d0cfebd1720d29cba8e8206fa32bc7e3f8
Bryton90 <waynekobe822@gmail.com>
2025-09-05 18:34:47 +0300

    Migrate app from JavaScript to TypeScript, update imports, add type annotations, and configure tsconfig for React compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,13 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import ts from '@typescript-eslint/eslint-plugin'
+import { defineConfig } from 'eslint/config';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
+    files: ['**/*.{ts,tsx}'],
+    parser: tsParser,
+    plugins: [ts],
+    extends: ['plugin:@typescript-eslint/recommended'],
   },
-])
+]);
